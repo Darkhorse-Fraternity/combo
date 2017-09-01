f93b64bfeb676754906a938f12f6e0828c825fbc
tonyYo <420156367@qq.com>
2017-09-01 16:22:44 +0800

    no message

---
+++ src/components/Nav/navigators/AppNavigator.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {addNavigationHelpers, StackNavigator} from 'react-navigation';
+
+
+import {route} from '../../../pages'
+export const AppNavigator = StackNavigator({
+    ...route,
+}, {
+    // initialRouteName:'Home',
+    navigationOptions: {
+        headerStyle:{
+            backgroundColor:'#F5FCFF',
+            shadowColor:'#F5FCFF',
+            borderBottomColor:'#F5FCFF',
+
+            //headerBackTitle:' '
+        },
+        headerTintColor:'black',
+        headerTitleStyle:{
+            color: 'black',
+            alignItems:'center',
+        },
+        headerBackTitle:'     ',
+
+    },
+});
+
+const AppWithNavigationState = ({dispatch, nav}) => (
+    <AppNavigator navigation={addNavigationHelpers({ dispatch, state: nav })}/>
+);
+
+AppWithNavigationState.propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    nav: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+    nav: state.nav,
+});
+
+export default connect(mapStateToProps)(AppWithNavigationState);
