fdab702c83e888d2fc3c48dbf7981a6657c1f57a
林通 <lintong320@gmail.com>
2019-11-28 09:48:26 +0800

    暂时切换临时的host, ts 配置替换

--- src/configure/reqConfigs.js
+++ src/configure/reqConfigs.js
@@ -5,17 +5,22 @@ import DeviceInfo from 'react-native-device-info';
 import { LeanCloud_APP_ID, LeanCloud_APP_SIGN } from './leancloud';
 import { appChannel } from '../../helps/util';
 
+// export const defaultHost = !__DEV__
+//   /* release */ ? 'api.icourage.cn/1.1'
+//   /* debug */ : 'api.icourage.cn/1.1';
 
 export const defaultHost = !__DEV__
-  /* release */ ? 'api.icourage.cn/1.1'
-  /* debug */ : 'api.icourage.cn/1.1';
+  ? /* release */ 'icard.leanapp.cn/1.1'
+  : /* debug */ 'icard.leanapp.cn/1.1';
 
+// export const apiHost = !__DEV__
+//   ? /* release */ "icourage.cn"
+//   : /* debug */ "stg-icard.leanapp.cn";
+// /* debug */ : 'icard.leanapp.cn';
 
 export const apiHost = !__DEV__
-  /* release */ ? 'icourage.cn'
-/* debug */ : 'stg-icard.leanapp.cn';
-  // /* debug */ : 'icard.leanapp.cn';
-
+  ? /* release */ 'icard.leanapp.cn'
+  : /* debug */ 'stg-icard.leanapp.cn';
 
 let LeanCloud_APP_Session = '';
 
@@ -23,26 +28,22 @@ export function setLeanCloudSession(session: string) {
   LeanCloud_APP_Session = session;
 }
 
-
-export function httpHeaders(needSession: bool): Object {
+export function httpHeaders(needSession: boolean): Object {
   const appVersion = DeviceInfo.getVersion();
 
   let header = {
     'Content-Type': 'application/json; charset=utf-8',
     'X-LC-Sign': LeanCloud_APP_SIGN,
     'X-LC-Id': LeanCloud_APP_ID,
-    'X-LC-Prod': __DEV__ ? 0
-      : 1,
+    'X-LC-Prod': __DEV__ ? 0 : 1,
     appVersion,
     appChannel
   };
 
   if (needSession) {
-    header = Object.assign({},
-      header,
-      {
-        'X-LC-Session': LeanCloud_APP_Session
-      });
+    header = Object.assign({}, header, {
+      'X-LC-Session': LeanCloud_APP_Session
+    });
   }
   // console.log('LeanCloud_APP_Session', LeanCloud_APP_Session);
   return header;
