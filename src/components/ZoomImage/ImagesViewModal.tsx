3d488bdd7ca4b6c22b0f305ef9f481c99177c37d
林通 <lintong320@gmail.com>
2020-05-13 17:55:39 +0800

    no message

--- src/components/ZoomImage/ImagesViewModal.tsx
+++ src/components/ZoomImage/ImagesViewModal.tsx
@@ -90,7 +90,7 @@ export default class ImagesViewModals extends Component {
     return (
       <Modal
         // useNativeDriver
-        deviceHeight={Dimensions.get('screen').height}
+
         style={{
           marginLeft: 0,
           marginRight: 0,
@@ -105,7 +105,13 @@ export default class ImagesViewModals extends Component {
           closeCallBack && closeCallBack();
         }}
         isVisible={visible}>
-        {Platform.OS !== 'ios' && <StatusBar backgroundColor="black" />}
+        {Platform.OS !== 'ios' && (
+          <StatusBar
+            backgroundColor="black"
+            barStyle={'light-content'}
+            translucent
+          />
+        )}
         <ImageViewer
           loadingRender={() => <ActivityIndicator />}
           imageUrls={imageUrls || []}
