d6905265cba9ef923523be1300cb1a50f9931d82
林通 <lintong320@gmail.com>
2020-11-30 17:13:31 +0800

    类型检测错误大体修复

--- src/components/Swipeable/GmailStyleSwipeableRow.tsx
+++ src/components/Swipeable/GmailStyleSwipeableRow.tsx
@@ -1,110 +1,111 @@
-// import React, { Component } from 'react';
-// import { Animated, StyleSheet, View } from 'react-native';
+//@ts-nocheck
 
-// import { RectButton } from 'react-native-gesture-handler';
+import React, { Component } from 'react';
+import { Animated, StyleSheet, View } from 'react-native';
 
-// import Swipeable from 'react-native-gesture-handler/Swipeable';
-// import Icon from 'react-native-vector-icons/MaterialIcons';
+import { RectButton } from 'react-native-gesture-handler';
 
-// const AnimatedIcon = Animated.createAnimatedComponent(Icon);
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// export default class AppleStyleSwipeableRow extends Component {
-//   renderLeftActions = (_, dragX) => {
-//     const trans = dragX.interpolate({
-//       inputRange: [0, 50, 100, 101],
-//       outputRange: [-20, 0, 0, 1],
-//     });
-//     return (
-//       <RectButton style={styles.leftAction} onPress={this.close}>
-//         <Animated.Text
-//           style={[
-//             styles.actionText,
-//             {
-//               transform: [{ translateX: trans }],
-//             },
-//           ]}>
-//           Archive
-//         </Animated.Text>
-//       </RectButton>
-//     );
-//   };
-//   renderRightAction = (progress, dragX) => {
-//     const scale = dragX.interpolate({
-//       inputRange: [-80, 0],
-//       outputRange: [1, 0],
-//       extrapolate: 'clamp',
-//     });
-//     const pressHandler = () => {
-//       // this.close();
-//       // this.props.onPress && this.props.onPress()
-//     };
-//     return (
-//       <RectButton style={styles.rightAction} onPress={pressHandler}>
-//         <AnimatedIcon
-//           name="delete-forever"
-//           size={30}
-//           color="#fff"
-//           style={[styles.actionIcon, { transform: [{ scale }] }]}
-//         />
-//       </RectButton>
-//     );
-//   };
-//   renderRightActions = (progress, data) => (
-//     <View style={{ flex: 1, flexDirection: 'row' }}>
-//       {data.map((item, index) => {
-//         const { backgroundColor, component, onPress } = item;
-//         return this.renderRightAction(
-//           component,
-//           backgroundColor,
-//           onPress,
-//           (data.length - index) * 64,
-//           progress,
-//           index,
-//         );
-//       })}
-//     </View>
-//   );
-//   updateRef = (ref) => {
-//     this._swipeableRow = ref;
-//   };
-//   close = () => {
-//     this._swipeableRow.close();
-//   };
+const AnimatedIcon = Animated.createAnimatedComponent(Icon);
 
-//   render() {
-//     const {
-//       children,
-//       rightThreshold,
-//       renderLeftActions,
-//       leftThreshold,
-//       right,
-//       ...otherProps
-//     } = this.props;
-//     return (
-//       <Swipeable
-//         ref={this.updateRef}
-//         friction={2}
-//         leftThreshold={leftThreshold}
-//         rightThreshold={rightThreshold || 10}
-//         renderLeftActions={renderLeftActions}
-//         renderRightActions={this.renderRightAction}
-//         {...otherProps}>
-//         {children}
-//       </Swipeable>
-//     );
-//   }
-// }
+export default class AppleStyleSwipeableRow extends Component {
+  renderLeftActions = (_, dragX) => {
+    const trans = dragX.interpolate({
+      inputRange: [0, 50, 100, 101],
+      outputRange: [-20, 0, 0, 1],
+    });
+    return (
+      <RectButton style={styles.leftAction} onPress={this.close}>
+        <Animated.Text
+          style={[
+            styles.actionText,
+            {
+              transform: [{ translateX: trans }],
+            },
+          ]}>
+          Archive
+        </Animated.Text>
+      </RectButton>
+    );
+  };
+  renderRightAction = (progress, dragX) => {
+    const scale = dragX.interpolate({
+      inputRange: [-80, 0],
+      outputRange: [1, 0],
+      extrapolate: 'clamp',
+    });
+    const pressHandler = () => {
+      // this.close();
+      // this.props.onPress && this.props.onPress()
+    };
+    return (
+      <RectButton style={styles.rightAction} onPress={pressHandler}>
+        <AnimatedIcon
+          name="delete-forever"
+          size={30}
+          color="#fff"
+          style={[styles.actionIcon, { transform: [{ scale }] }]}
+        />
+      </RectButton>
+    );
+  };
+  renderRightActions = (progress, data) => (
+    <View style={{ flex: 1, flexDirection: 'row' }}>
+      {data.map((item, index) => {
+        const { backgroundColor, component, onPress } = item;
+        return this.renderRightAction(
+          component,
+          backgroundColor,
+          onPress,
+          (data.length - index) * 64,
+          progress,
+          index,
+        );
+      })}
+    </View>
+  );
+  updateRef = (ref) => {
+    this._swipeableRow = ref;
+  };
+  close = () => {
+    this._swipeableRow.close();
+  };
 
-// const styles = StyleSheet.create({
-//   actionIcon: {
-//     width: 30,
-//     marginHorizontal: 10,
-//   },
-//   leftAction: {},
-//   rightAction: {
-//     alignItems: 'flex-end',
-//     backgroundColor: '#dd2c00',
-//     flex: 1,
-//     justifyContent: 'center',
-//   },
-// });
+  render() {
+    const {
+      children,
+      rightThreshold,
+      renderLeftActions,
+      leftThreshold,
+      ...otherProps
+    } = this.props;
+    return (
+      <Swipeable
+        ref={this.updateRef}
+        friction={2}
+        leftThreshold={leftThreshold}
+        rightThreshold={rightThreshold || 10}
+        renderLeftActions={renderLeftActions}
+        renderRightActions={this.renderRightAction}
+        {...otherProps}>
+        {children}
+      </Swipeable>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  actionIcon: {
+    width: 30,
+    marginHorizontal: 10,
+  },
+  leftAction: {},
+  rightAction: {
+    alignItems: 'flex-end',
+    backgroundColor: '#dd2c00',
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
--- src/components/Update/EPUpdate.tsx
+++ src/components/Update/EPUpdate.tsx
@@ -1,3 +1,4 @@
+//@ts-nocheck
 import React from 'react';
 
 import DeviceInfo from 'react-native-device-info';
